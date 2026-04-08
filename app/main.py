a2efb56463feb48672bea0c52e431a561d0d76a2
Celine <272948357+mxxcissie@users.noreply.github.com>
2026-04-08 16:07:22 -0700

    Update CORS for deployed frontend

--- app/main.py
+++ app/main.py
@@ -30,6 +30,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
+        "https://url-shortener-frontend-av1x.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
